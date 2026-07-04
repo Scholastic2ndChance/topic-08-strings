76a0fa5535d63b6e69e4747179087ac06fd3640e
Scholastic2ndChance <bmoore@edisonohio.edu>
2026-07-03 23:26:45 -0400

    Create christmas.py
    Created the file and started building the code.

---
+++ christmas.py
@@ -0,0 +1,81 @@
+# create functions
+
+# Print the menu
+def menu():
+  pass
+
+# Print the full list
+def full_gift_list():
+  pass
+
+# Code to alphebitize the full dictionary or partial dictionary
+def alphebitize():
+  pass
+
+# Code to alphebitize the full dictionary using the other function
+def alphebitize_full():
+  pass
+
+# Print person and their gifts
+def person_gift():
+  pass
+
+# Type gift and return recipiant
+def gift_who():
+  pass
+
+# Modify the gift list by either adding, changing, or removing
+def gift_modify():
+  pass
+
+  def gift_add():
+    pass
+  
+  def gift_fix():
+    pass
+  
+  def gift_delete():
+    pass
+  
+  def gift_move():
+    pass
+
+# Run through the quit sequence
+def quit():
+  pass
+
+
+# initialize Dictionaries.
+user_menu = {
+  "1. Print Full List" : ["1", "value"],
+  "2. Print Full Inventory" : "value",
+  "5. Search Feature" : "",
+  "4. Modify Gift" : "",
+  "6. Exit" : ""
+}
+
+gift_tracker = {
+    "Dad": ["New drill bits", "Coffee sampler"],
+    "Mom": ["Knitting yarn", "Spa gift card"],
+    "Sister": ["Cookbook", "Bluetooth speaker"],
+    "Brother": ["Board game", "Running socks"],
+    "Grandma": ["Puzzle set", "Tea assortment"],
+    "Grandpa": ["History book", "Warm slippers"]
+}
+
+quit_codes = ("quit", "q", "done", "d", "exit", "e")
+
+# Non-Function Code (call functions based on choices)
+quit = None
+while quit != quit_codes:
+  menu(user_menu):
+
+  
+
+
+
+
+else:
+  Print("Goodbye!"
+
+
